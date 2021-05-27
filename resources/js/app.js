0567cea630d338dcac26b959e5aacd8a082747e7
kouyate karim <karimkouyate@adjemin.com>
2021-05-27 20:11:20 +0000

    init

--- resources/js/app.js
+++ resources/js/app.js
@@ -35,10 +35,10 @@ import Echo from 'laravel-echo';
 
  
 
- window.Echo = new Echo({
+/*  window.Echo = new Echo({
    broadcaster: 'socket.io',
     host : window.location.hostname + ':6001'
  });
-
+ */
 
 
